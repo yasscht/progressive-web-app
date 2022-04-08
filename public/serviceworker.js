5a6a9c5e9194d114f59aaaf6fdef6156fb411b7c
yassine.chtiaa@etu.uae.ac.ma <yassine.chtiaa@etu.uae.ac.ma>
2022-04-08 13:12:52 +0000

    add the service worker file to the app

---
+++ public/serviceworker.js
@@ -0,0 +1,41 @@
+const CACHE_NAME = "version-1";
+const urlsToCache = ["index.html", "offline.html"];
+const self = this;
+//installing the service worker
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log("cache opened");
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Listen for the request
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches
+      .match(event.request)
+      .then(() => {
+        return fetch(event.request);
+      })
+      .catch(() => caches.match("offline.html"))
+  );
+});
+
+// Activate the service worker
+self.addEventListener("activate", (event) => {
+  const cacheWhitelist = [];
+  cacheWhitelist.push(CACHE_NAME);
+  event.waitUntil(
+    caches.keys().then((cacheNames) =>
+      Promise.all(
+        cacheNames.map((cacheName) => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      )
+    )
+  );
+});
